0bd7e4a8702a4abb789cfb7048d98abf54907588
FaheemBhatti <mefaheemazfar@gmail.com>
2020-08-07 11:32:39 +0200

    Updated Main

--- src/org/Composition/Main/Main.java
+++ src/org/Composition/Main/Main.java
@@ -6,14 +6,15 @@ import org.Composition.Laptop.Laptop;
 
 public class Main 
 {
-	
 	public static void main(String[] args)
 	{
 		Processor processor = new Processor("Intel", "i5 72000u", "7th", 4, 8, 16, 3.1f);
 		Graphicscard graphicsCard = new Graphicscard("Nvidia", "GTX", "16GB");
 		
-		Laptop gammingLaptop = new Laptop(17.6f, "32GB", 2, "backlit", processor, graphicsCard);
-		System.out.println(gammingLaptop.toString());
+		Laptop laptop = new Laptop(15.7f, "32GB" , 10000, "Backled" , new Processor() , new Graphicscard());
+		System.out.println(laptop);
 		
+		Laptop gammingLaptop = new Laptop(17.6f, "32GB", 2, "backlit", new Processor(), new Graphicscard());
+		System.out.println(gammingLaptop.toString());		
 	}
 }
